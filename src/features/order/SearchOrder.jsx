8044574a56cde6094ae24cf6ca49ce6bccdac4c4
Imoesi Rasheed Izuagbe <rasheedjay13@gmail.com>
2025-08-10 09:47:08 +0100

    Lets goo

---
+++ src/features/order/SearchOrder.jsx
@@ -0,0 +1,31 @@
+import React, { useState } from 'react'
+import { useNavigate } from 'react-router-dom';
+
+
+
+
+export default function SearchOrder() {
+    const [query, setQuery] = useState("");
+    const navigate = useNavigate();
+
+    function handleSubmit(e) {
+        e.preventDefault();
+        if(!query) return;
+        // Logic to search for orders based on the query
+        navigate(`/order/${query}`);
+        setQuery("");
+    }
+    
+    return (
+    <form onSubmit={handleSubmit}>
+
+    <input
+        placeholder='Search'
+        value={query}
+        onChange={(e) => setQuery(e.target.value)} 
+    />
+    
+    
+    </form>
+  );
+}
---
+++ src/ui/Header.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import { Link } from 'react-router-dom'
+
+export default function Header() {
+  return (
+    <header>
+        <Link to="/">Heed Pizza</Link>
+        <p>Rasheed</p>
+    </header>
+  )
+}
