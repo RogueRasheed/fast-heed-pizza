a8b8eb9988bac6947c5247d587ae523a37313fcc
Imoesi Rasheed Izuagbe <rasheedjay13@gmail.com>
2025-08-19 02:41:48 +0100

    update commit

--- src/features/order/CreateOrder.jsx
+++ src/features/order/CreateOrder.jsx
@@ -1,12 +1,11 @@
-import { useState } from "react";
 import React from "react";
-import { Form, redirect } from "react-router-dom";
+import { Form, redirect, useActionData, useNavigation } from "react-router-dom";
 import { createOrder } from "../../services/apiRestaurant";
 // https://uibakery.io/regex-library/phone-number
-const isValidPhone = (str) =>
-  /^\+?\d{1,4}?[-.\s]?\(?\d{1,3}?\)?[-.\s]?\d{1,4}[-.\s]?\d{1,4}[-.\s]?\d{1,9}$/.test(
-    str
-  );
+ const isValidPhone = (str) =>
+   /^\+?\d{1,4}?[-.\s]?\(?\d{1,3}?\)?[-.\s]?\d{1,4}[-.\s]?\d{1,4}[-.\s]?\d{1,9}$/.test(
+     str
+   );
 
 const fakeCart = [
   {
@@ -33,6 +32,11 @@ const fakeCart = [
 ];
 
 function CreateOrder() {
+  const navigation = useNavigation();
+  const isSubmitting = navigation.state === "submitting";
+
+  const formErrors = useActionData();
+
   // const [withPriority, setWithPriority] = useState(false);
   const cart = fakeCart;
 
@@ -51,6 +55,7 @@ function CreateOrder() {
           <div>
             <input type="tel" name="phone" required />
           </div>
+          {formErrors?.phone && <p>{formErrors.phone}</p>}
         </div>
 
         <div>
@@ -77,7 +82,7 @@ function CreateOrder() {
             name="cart"
             value={JSON.stringify(cart)}
           />
-          <button>Order now</button>
+          <button disabled={isSubmitting}>{isSubmitting ? "Ordering..." : "Order now"}</button>
         </div>
       </Form>
     </div>
@@ -96,7 +101,14 @@ export async function action({request}){
     priority: data.priority === "on",
   };
 
+  // if everything is all good, create new order.
   const newOrder = await createOrder(order);
+
+  const errors = {}
+  if(!isValidPhone(order.phone)) errors.phone = "Please enter a valid phone number";
+  if(Object.keys(errors).length > 0) {
+    return { errors };
+  }
 
   return redirect(`/order/${newOrder.id}`);
 }
--- src/ui/Home.jsx
+++ src/ui/Home.jsx
@@ -1,11 +1,16 @@
+import React from 'react';
+import CreateUser from '../features/user/CreateUser';
 function Home() {
   return (
     <div>
-      <h1>
-        The best pizza.
+      <h1 className="text-center text-xl font-bold">
+        The Best Pizza.
         <br />
-        Straight out of the oven, straight to you.
+        <span className="text-yellow-500"> Delivered fast.</span>
+        <br />
+        <span className="text-stone-500"> Straight out of the oven, Straight to you.</span>
       </h1>
+      <CreateUser />
     </div>
   );
 }
